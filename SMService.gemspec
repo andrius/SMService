4f6eec6b43c9935a74b3b4d995c64bf396ccd34a
Andrius Kairiukstis <k@andrius.mobi>
2017-11-17 18:21:35 +0100

    gem-ification started

---
+++ SMService.gemspec
@@ -0,0 +1,41 @@
+lib = File.expand_path("../lib", __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require "SMService/version"
+
+Gem::Specification.new do |s|
+  s.name          = "SMService"
+  s.version       = SMService::VERSION
+  s.authors       = ["Andrius Kairiukstis"]
+  s.email         = ["k@andrius.mobi"]
+
+  s.summary       = %q{Service Manager client.}
+  s.description   = %q{Service Manager client with ZeroMQ DEALER endpoints, to build voice test automation actions based on ruby.}
+  s.homepage      = "https://github.com/andrius/smservice"
+  s.license       = "MIT"
+
+  # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
+  # to allow pushing to a single host or delete this section to allow pushing to any host.
+  # if s.respond_to?(:metadata)
+  #   s.metadata["allowed_push_host"] = "https://rubygems.org"
+  # else
+  #   raise "RubyGems 2.0 or newer is required to protect against " \
+  #     "public gem pushes."
+  # end
+
+  s.files         = `git ls-files -z`.split("\x0").reject do |f|
+    f.match(%r{^(test|spec|features)/})
+  end
+  # s.bindir        = "bin"
+  # s.executables   = s.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  s.executables = []
+  s.require_paths = ["lib"]
+ 
+  s.add_runtime_dependency "logger"
+  s.add_runtime_dependency "msgpack", "~> 1.1"
+  s.add_runtime_dependency "ffi-rzmq", "~> 2.0"
+
+
+  s.add_development_dependency "bundler", "~> 1.16"
+  s.add_development_dependency "rake", "~> 10.0"
+  s.add_development_dependency "rspec", "~> 3.0"
+end
--- lib/smservice.rb
+++ lib/smservice.rb
@@ -1,5 +1,141 @@
+require "timeout"
 require "SMService/version"
 
 module SMService
-  # Your code goes here...
+  LOGGER = Logger.new(STDERR)
+
+  ENDPOINT_OUT = 'tcp://proxy:6660'
+  ENDPOINT_IN = 'tcp://proxy:6661'
+
+  REGISTER_WAIT_TIME = 'infinite'
+  KEEPALIVE_WAIT_TIME = 30
+
+  attr_accessor :socket_in, :socket_out
+  attr_reader :service_name
+
+  def initialize(name:)
+    @service_name = name
+
+    context = ZMQ::Context.new
+    @socket_in = context.socket ZMQ::DEALER
+    @socket_out = context.socket ZMQ::DEALER
+
+    @socket_in.setsockopt ZMQ::IDENTITY, service_name
+    @socket_in.connect ENDPOINT_IN
+
+    @socket_out.setsockopt ZMQ::IDENTITY, service_name
+    @socket_out.connect ENDPOINT_OUT
+  end
+
+  def register!(wait_time: REGISTER_WAIT_TIME)
+    if wait_time.to_f <= 0
+      loop do
+        register!
+        break if registered?
+        # not registered, retrying in 10 seconds
+        sleep 10
+      end
+    else
+      Timeout::timeout(wait_time) do
+        loop do
+          register!
+          break if registered?
+          # not registered, retrying in 10 seconds
+          sleep 10
+        end
+      end
+    end
+  end
+
+  def keep_alive!(wait_time: KEEPALIVE_WAIT_TIME)
+    Thread.start do
+      LOGGER.info "Starting registration update loop with periodic interval #{wait_time} sec"
+      loop do
+        break unless registered?
+        sleep wait_time
+        service_manager('UPDATE',  [])
+      end
+      LOGGER.info 'Registration update loop terminated'
+    end
+  end
+
+  def registered?
+    @registered
+  end
+
+  def register!
+    service_manager('REGISTER',  services: [service_name])
+    pull_action
+  end
+
+  def service_poller
+    loop do
+      pull_action
+    end
+  end
+
+  def start!
+    register!
+    keep_alive!
+    service_poller
+  end
+
+  def action_update(headers, message)
+    LOGGER.info("Action: UPDATE (successful)")
+  end
+
+  def action_register(headers, message)
+    if headers['action'] == 'REGISTER' && message['result'] == 'OK'
+      @registered = true
+      LOGGER.info "Action: REGISTER (successful), headers: #{headers.inspect}, message: #{message.inspect}"
+    else
+      LOGGER.info "Action: REGISTER (failure), headers: #{headers.inspect}, message: #{message.inspect}"
+    end   
+  end
+
+private
+
+  # Sending request to the Service Manager
+  # 
+  def service_manager(action, message)
+    LOGGER.info "SM request. Action: #{action.inspect}, message: #{message.inspect}"
+    action = {action: action}.to_msgpack
+    message = message.to_msgpack
+    @socket_in.send_strings [action, message]
+  end
+
+  def pull_action
+    @socket_in.recv_strings( response = [] )
+
+    headers, message = response
+    headers = MessagePack.unpack(headers)
+    message = MessagePack.unpack(message)
+
+    LOGGER.info "SM response received by pull_action. Headers: #{headers.inspect}, message: #{message.inspect}"
+
+    # Validates is action_name exists as class method and calling it.
+    # In case, if Service Manager return headers['action'] == 'METHOD_NAME',
+    # ruby expecting that somethinf like this would be defined:
+    #
+    # def action_method_name(headers, message)
+    #   # ... some business logic here
+    # end
+    #
+    # Two actions defined as part of module, it is :
+    # - action_register, that confirms registration of node with Service Manager, and
+    # - action_update, that confirms successfule registration update, we need it to support keep-alive
+    #
+    action_name = "action_#{headers['action'].downcase}".to_sym
+    if respond_to? action_name.to_sym
+      send(action_name, headers, message)
+    end
+  end
+end
+
+class SMService::Dummy
+  include SMService
+
+  def initialize
+    super name: 'dummy-ruby'
+  end
 end
